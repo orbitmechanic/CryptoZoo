3171501c2d2b97f1366ba3405eec0453714164dd
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-10-21 16:30:18 -0700

    draft test.

---
+++ test/token_test.js
@@ -0,0 +1,13 @@
+const CryptAnimal = artifacts.require("Cryptanimal");
+const { iteratee } = require("lodash");
+const truffleAssert = require("truffle-assertions");
+
+contract("CryptAnimal", async function() {
+    it("Total supply should begin at zero", async function() {
+        let instance = await CryptAnimal.deployed();
+        await truffleAssert.passes(
+            instance.totalSupply(),
+            'totalSupply() should run.'
+        );
+    })
+})
